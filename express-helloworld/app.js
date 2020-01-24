61d5affea7d2d5de2e2fa9c7a9e83d00b8661d74
zapatafs <faleg.zapata@mail.telcel.com>
2020-01-24 17:48:58 -0600

    cambio 4

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,8 +1,14 @@
 var express = require('express');
 app = express();
 
+var response;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!\n');
+  response = 'This is version 2 of the app.' + '\n';
+
+  //send the response to the client
+  res.send(response);
+
 });
 
 app.get('/mars', function(req, res) {
@@ -11,14 +17,4 @@ app.get('/mars', function(req, res) {
 
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
-});
-
-var response;
-
-app.get('/', function (req, res) {
-    response = 'This is version 2 of the app.' + '\n';
-
-    //send the response to the client
-    res.send(response);
-
 });
