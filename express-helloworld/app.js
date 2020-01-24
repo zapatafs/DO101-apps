36952c84889f2971b4f90bfddc7e923e162d35d5
zapatafs <faleg.zapata@mail.telcel.com>
2020-01-24 17:13:46 -0600

    updated app to v2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -13,3 +13,12 @@ app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
 
+var response;
+
+app.get('/', function (req, res) {
+    response = 'This is version 2 of the app.' + '\n';
+
+    //send the response to the client
+    res.send(response);
+
+});
